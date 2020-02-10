fed805ac889e0c5ad863064669a0a124c54cd60e
Heeseok <heidenletter@gmail.com>
2020-02-10 16:47:05 +0900

    first commit

---
+++ src/Robot_comm/include/robot/chic_m4k.hpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+class Chic_m4k
+{
+    private:
+    struct Joy_msg
+    {
+        unsigned int seq;
+        ros::Time stamp;
+        std::string frame_id;
+
+        
+    };
+};
